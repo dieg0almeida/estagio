1d2434a844b331cf44f351673cce451df65a26ab
Diego Almeida <diegalmeida@hotmail.com>
2020-04-02 22:40:13 -0300

    dotenv example

--- src/config/db.js
+++ src/config/db.js
@@ -1,11 +1,11 @@
 const mysql = require('mysql2');
 
 const pool = mysql.createPool({
-    host: 'localhost',
-    //port: process.env.DB_PORT,
-    user: 'root',
-    database: 'estagio',
-   // password: process.env.DB_PASSWORD,
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    user: process.env.DB_USER,
+    database: process.env.DB_NAME,
+    password: process.env.DB_PASSWORD,
     waitForConnections: true,
     connectionLimit: 10,
     queueLimit: 0
